26ee1318a3f41bcb5c68ea079eeffbc0592950d5
Paul Thomas <paulthomas0824@gmail.com>
2020-08-18 11:36:36 -0500

    Update Bubble_Sort.cpp

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -1,8 +1,8 @@
 #include <iostream> 
 using namespace std;
 
-template <class T>
-void bubbleSort(T a[], int n) {
+template <class B>
+void bubbleSort(B a[], int n) {
 	for (int i = 0; i < n - 1; i++)
 		for (int j = n - 1; i < j; j--)
 			if (a[j] < a[j - 1])
